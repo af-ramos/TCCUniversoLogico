619066e3e9699710297692d7a53577190ce881dd
af-ramos <arfrra@hotmail.com>
2022-12-27 14:06:12 -0300

    Adicionando projeto...

---
+++ lib/DADOS/carregamentoImagens/carregamentoImagens.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class Imagens
+{
+  //TELA DE JOGOS
+  static Image fundoJogos;
+  static Image capaJogoParImpar;
+  static Image capaJogoVelha;
+  static Image capaJogoOrdenarNumeros;
+  //FUNDO JOGOS
+  static Image fundoParImpar;
+  static Image fundoJogoVelha;
+  static Image fundoSelecaoOrdenarNumeros;
+  static Image fundoJogoOrdenarNumeros;
+}
---
+++ lib/MENU/telaJogos/data.dart
@@ -0,0 +1,26 @@
+import 'package:flutter/material.dart';
+import 'package:universo_logico/DADOS/carregamentoImagens/carregamentoImagens.dart';
+
+class IntroItem {
+  IntroItem({
+    this.title,
+    this.category,
+    this.imageGame,
+    this.buttonText,
+    this.urlButton,
+    this.itemColor,
+  });
+
+  final String title;
+  final String category;
+  final Image imageGame;
+  final String buttonText;
+  final String urlButton;
+  final Color itemColor;
+}
+
+final sampleItems = <IntroItem>[
+  new IntroItem(title: 'Pense rapidamente e me diga se é par ou ímpar!', category: 'PAR E ÍMPAR', imageGame: Imagens.capaJogoParImpar, buttonText: 'Jogar', urlButton: '/par&Impar', itemColor: Colors.red),
+  new IntroItem(title: 'Monte uma sequência para ganhar o jogo!', category: 'JOGO DA VELHA', imageGame: Imagens.capaJogoVelha, buttonText: 'Jogar', urlButton: '/jogoDaVelha', itemColor: Colors.lightBlue),
+  new IntroItem(title: 'Ordene os números na sequência correta!', category: 'ORDENAR NÚMEROS', imageGame: Imagens.capaJogoOrdenarNumeros, buttonText: 'Jogar', urlButton: '/selecaoOrdenarNumeros', itemColor: Colors.deepOrange),
+];
